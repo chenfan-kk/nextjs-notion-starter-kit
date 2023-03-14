10814df71d0fb629a5c2de7d230500d53df264c6
chenfan-kk <125135568+chenfan-kk@users.noreply.github.com>
2023-03-14 22:30:55 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '5a28d72e6e474d7eb33b8ae9fbc3d8f6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '工具鸭bot',
+  domain: 'https://www.notion.so/5a28d72e6e474d7eb33b8ae9fbc3d8f6',
+  author: '小鸭',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
